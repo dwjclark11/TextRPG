87af870421aa04c5f5782c4e3949b8418a946847
Dustin Clark <dwjclark1@hotmail.com>
2023-01-06 10:06:45 -0500

    Implemented Selector class and add NOMINMAX preprocess definition

--- Text_RPG_cpp/src/Selector.h
+++ Text_RPG_cpp/src/Selector.h
@@ -45,11 +45,14 @@ private:
 	int m_Rows;
 
 public:
-	Selector();
+	Selector(Console& console, Keyboard& keyboard, std::vector<T> data, SelectorParams params = SelectorParams());
+	Selector(Console& console, Keyboard& keyboard, 
+		std::function<void(int, std::vector<T>)> on_selection, 
+		std::function<void(int, int, T)> on_draw_item, std::vector<T> data, SelectorParams params = SelectorParams());
 	~Selector();
 
-	void SetData(std::vector<T> data) { mData = data; }
-	const std::vector<T>& GetData() { return mData; }
+	void SetData(std::vector<T> data) { m_Data = data; }
+	const std::vector<T>& GetData() { return m_Data; }
 	void SetSelectionFunc(std::function<void(int, std::vector<T>)> on_selection) { m_OnSelection = on_selection; }
 	void SetDrawFunc(std::function<void(int, int, T)> on_draw_item) { m_OnDrawItem = on_draw_item; }
 	void ShowCursor() { m_bShowCursor = true; }
@@ -68,3 +71,163 @@ public:
 
 	void Draw();
 };
+
+template<typename T>
+inline Selector<T>::Selector(Console& console, Keyboard& keyboard, std::vector<T> data, SelectorParams params)
+	: Selector(
+		console, 
+		keyboard, 
+		[this](int index, std::vector<T> data) {Selector::OnSelection(index, data); },		 
+		[this](int x, int y, T item) {Selector::DrawItem(x, y, item); },
+		data, params)
+{
+
+}
+
+template<typename T>
+inline Selector<T>::Selector(Console& console, Keyboard& keyboard,
+	std::function<void(int, std::vector<T>)> on_selection,
+	std::function<void(int, int, T)> on_draw_item, std::vector<T> data, SelectorParams params)
+	: m_Console(console)
+	, m_Keyboard(keyboard)
+	, m_OnSelection(on_selection)
+	, m_OnDrawItem(on_draw_item)
+	, m_Data(data)
+	, m_Params(params)
+	, m_bShowCursor(true)
+{
+	// Initialize the number of rows 
+	m_Rows = std::ceil(m_Data.size() / (params.columns == 0 ? 1 : params.columns));
+
+	// Check to see if rows is < 1
+	if (m_Rows < 1)
+		m_Rows = 1;
+}
+
+template<typename T>
+inline Selector<T>::~Selector()
+{
+
+}
+
+template<typename T>
+inline void Selector<T>::ProcessInputs()
+{
+	if (m_Keyboard.IsKeyJustPressed(KEY_W))
+		MoveUp();
+	else if (m_Keyboard.IsKeyJustPressed(KEY_S))
+		MoveDown();
+	else if (m_Keyboard.IsKeyJustPressed(KEY_A))
+		MoveLeft();
+	else if (m_Keyboard.IsKeyJustPressed(KEY_D))
+		MoveRight();
+	else if (m_Keyboard.IsKeyJustPressed(KEY_SPACE))
+		OnAction();
+}
+
+template<typename T>
+inline void Selector<T>::MoveUp()
+{
+	m_Params.currentY = std::max(m_Params.currentY - 1, 0);
+}
+
+template<typename T>
+inline void Selector<T>::MoveDown()
+{
+	m_Params.currentY = std::min(m_Params.currentY + 1, m_Rows - 1);
+}
+
+template<typename T>
+inline void Selector<T>::MoveLeft()
+{
+	m_Params.currentX = std::max(m_Params.currentX - 1, 0);
+}
+
+template<typename T>
+inline void Selector<T>::MoveRight()
+{
+	m_Params.currentX = std::max(m_Params.currentX + 1, m_Params.columns - 1);
+}
+
+template<typename T>
+inline void Selector<T>::OnAction()
+{
+	int index = GetIndex();
+	m_OnSelection(index, m_Data);
+}
+
+template<typename T>
+inline void Selector<T>::DrawItem(int x, int y, T item)
+{
+	// Check to see if the type is wstring
+	if constexpr (std::is_same<T, std::wstring>::value)
+	{
+		m_Console.Write(x, y, item);
+		return;
+	}
+
+	// Get the type and let user know to override
+	std::string type = typeid(item).name();
+	TRPG_ERROR("Data type [" + type + "] - needs to be a wstring, Please create the DrawItem function to override!");
+}
+
+// This function should be overriden
+template<typename T>
+inline void Selector<T>::OnSelection(int index, std::vector<T> data)
+{
+	m_Console.Write(50, 20, L"Index: " + std::to_wstring(index));
+}
+
+template<typename T>
+inline void Selector<T>::Draw()
+{
+	// If there is not data, nothing to draw
+	if (m_Data.empty())
+		return;
+
+	int itemIndex = 0;
+	int x = m_Params.x;
+	int y = m_Params.y;
+	int rowHeight = m_Params.spacingY;
+	int spacingX = m_Params.spacingX;
+
+	int maxData = m_Data.size();
+
+	for (int i = 0; i < m_Rows; i++)
+	{
+		for (int j = 0; j < m_Params.columns; j++)
+		{
+			if (i == m_Params.currentY && j == m_Params.currentX)
+			{
+				if (m_bShowCursor)
+				{
+					// Reset the areas behind the cursor as it moves
+					if (i != 0)
+						m_Console.Write(x - (x == 0 ? 0 : 2), y - rowHeight, L" ");
+
+					m_Console.Write(x - (x == 0 ? 0 : 2), y + rowHeight, L" ");
+					m_Console.Write(x - (x == 0 ? 0 : 2) - spacingX, y, L" ");
+					m_Console.Write(x - (x == 0 ? 0 : 2) + spacingX, y, L" ");
+
+					// Draw the cursor
+					m_Console.Write(x - (x == 0 ? 0 : 2), y, m_Params.cursor, RED);
+				}
+				else
+				{
+					// Clear the cursor
+					m_Console.Write(x - (x == 0 ? 0 : 2), y, L" ");
+				}
+			}
+
+			if (itemIndex < maxData)
+			{
+				T item = m_Data[itemIndex];
+				m_OnDrawItem(x, y, item);
+				x += spacingX;
+				itemIndex++;
+			}
+		}
+		y += rowHeight;
+		x = m_Params.x;
+	}
+}
