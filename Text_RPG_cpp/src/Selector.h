013bb1cd4d7077e21e1088731810342490ae62d7
Dustin Clark <dwjclark1@hotmail.com>
2023-01-27 10:23:42 -0500

    Changed the selector clearing to be based on the current X and Y

--- Text_RPG_cpp/src/Selector.h
+++ Text_RPG_cpp/src/Selector.h
@@ -227,12 +227,17 @@ inline void Selector<T>::Draw()
 				if (m_bShowCursor)
 				{
 					// Reset the areas behind the cursor as it moves
-					if (i != 0)
+					if (m_Params.m_CurrentY != 0)
 						m_Console.Write(x - (x == 0 ? 0 : 2), y - rowHeight, L" ");
 
-					m_Console.Write(x - (x == 0 ? 0 : 2), y + rowHeight, L" ");
-					m_Console.Write(x - (x == 0 ? 0 : 2) - spacingX, y, L" ");
-					m_Console.Write(x - (x == 0 ? 0 : 2) + spacingX, y, L" ");
+					if (m_Params.m_CurrentX != 0)
+						m_Console.Write(x - (x == 0 ? 0 : 2) - spacingX, y, L" ");
+
+					if (m_Params.m_CurrentY != m_Rows - 1)
+						m_Console.Write(x - (x == 0 ? 0 : 2), y + rowHeight, L" ");
+
+					if (m_Params.m_CurrentX != m_Params.m_Columns - 1)
+						m_Console.Write(x - (x == 0 ? 0 : 2) + spacingX, y, L" ");
 
 					// Draw the cursor
 					m_Console.Write(x - (x == 0 ? 0 : 2), y, m_Params.cursor, RED);
