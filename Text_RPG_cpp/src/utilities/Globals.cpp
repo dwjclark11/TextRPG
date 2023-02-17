901830168a333919c152e41d7bf8b9eda6544215
Dustin Clark <dwjclark1@hotmail.com>
2023-02-16 19:40:31 -0500

    Added TRPG_Globals as a game data class

---
+++ Text_RPG_cpp/src/utilities/Globals.cpp
@@ -0,0 +1,37 @@
+#include "Globals.h"
+#include "trpg_utilities.h"
+
+std::unique_ptr<TRPG_Globals> TRPG_Globals::m_pInstance = nullptr;
+
+TRPG_Globals::TRPG_Globals()
+    : m_GameTime{0}
+    , m_SavedGameTime{0}
+    , m_Timer{}
+
+{
+    m_Timer.Start();
+}
+
+TRPG_Globals& TRPG_Globals::GetInstance()
+{
+    if (!m_pInstance)
+        m_pInstance.reset(new TRPG_Globals());
+
+    return *m_pInstance;
+}
+
+void TRPG_Globals::Update()
+{
+    m_GameTime = m_Timer.ElapsedSec() + m_SavedGameTime;
+}
+
+const std::wstring TRPG_Globals::GetTime()
+{
+    int hours = m_GameTime / 3600;
+    int minutes = (m_GameTime % 3600) / 60;
+    int seconds = m_GameTime % 60;
+
+    std::wstring time{ PadNumbers(hours) + L":" + PadNumbers(minutes) + L":" + PadNumbers(seconds) };
+
+    return time;
+}
---
+++ Text_RPG_cpp/src/utilities/Globals.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <memory>
+#include <string>
+#include "Timer.h"
+
+class TRPG_Globals
+{
+private:
+	int m_GameTime, m_SavedGameTime;
+	Timer m_Timer;
+
+	TRPG_Globals();
+
+	static std::unique_ptr<TRPG_Globals> m_pInstance;
+public:
+	static TRPG_Globals& GetInstance();
+
+	const int GetGameTime() const { return m_GameTime; }
+	void SetSaveGameTime(int saved_time) { m_SavedGameTime = saved_time; }
+	void Update();
+	const std::wstring GetTime();
+};
