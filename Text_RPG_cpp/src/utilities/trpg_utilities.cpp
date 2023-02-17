21ffde3244c9ed09557f6f93310d9262fe3bf011
Dustin Clark <dwjclark1@hotmail.com>
2023-02-16 19:39:58 -0500

    Added Utility functions
    Added three helper utility functions to aid in the game
    - CharToWide() - Convert const char* to wstring
    - WideToStr() - Convert wstring to string
    - PadNumbers() - Add a zero in front of numbers < 10

---
+++ Text_RPG_cpp/src/utilities/trpg_utilities.cpp
@@ -0,0 +1,22 @@
+#include "trpg_utilities.h"
+
+std::wstring CharToWide(const char* str)
+{
+    return std::wstring(str, str + strlen(str));
+}
+
+std::string WideToStr(const std::wstring& wstr)
+{
+    int size_in_bytes = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    std::string strTo(size_in_bytes, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_in_bytes, NULL, NULL);
+    return strTo;
+}
+
+std::wstring PadNumbers(int num)
+{
+    if (num < 10)
+        return L"0" + std::to_wstring(num);
+
+    return std::to_wstring(num);
+}
--- Text_RPG_cpp/src/utilities/trpg_utilities.h
+++ Text_RPG_cpp/src/utilities/trpg_utilities.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
+#include <string>
+#include <Windows.h>
 
 template <typename T>
 void remove(std::vector<T>& vec, size_t index)
 {
 	vec.erase(vec.begin() + index);
 }
+
+std::wstring CharToWide(const char* str);
+std::string WideToStr(const std::wstring& wstr);
+std::wstring PadNumbers(int num);
